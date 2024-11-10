6d85a73ea28374ebb6bfd50910f587098e76c580
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-11-10 23:37:09 +0200

    make changes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  // Ensure Flutter's binding is initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
   // Load environment variables
   await dotenv.load();
 
-  // Initialize Supabase with the API key and URL from .env
+  //Initialize Supabase with the API key and URL from .env
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+
   
   runApp(CarDiagnosticsApp());
 }
