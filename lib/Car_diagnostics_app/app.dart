b46b365234ac1c431e1ec47e381e820c0b86beb1
Mostafa-Awaad <mostafamohamedkamal2000@gmail.com>
2024-12-01 01:14:20 +0200

    Add changes to app theme

--- lib/Car_diagnostics_app/app.dart
+++ lib/Car_diagnostics_app/app.dart
@@ -15,14 +15,14 @@ class CarDiagnosticsApp extends StatelessWidget {
         primaryColor: Color(0xFF00FFC6), // Primary color, a bright neon green-cyan
 
         // Dark color scheme for the theme, with custom primary and secondary colors
-        colorScheme: ColorScheme.dark(
+        colorScheme: const ColorScheme.dark(
           primary: Color(0xFF00FFC6), // Matches primaryColor above
           secondary: Color(0xFF29B6F6), // Neon blue used for secondary elements
-          background: Color(0xFF1B1B1F), // Background color for app screens
+          surface: Color(0xFF1B1B1F), // Background color for app screens
         ),
 
         // Icon theme settings for general app icons
-        iconTheme: IconThemeData(
+        iconTheme: const IconThemeData(
           color: Colors.blueGrey, // Muted gray color for non-primary icons
           size: 30, // Larger icon size
         ),
@@ -42,14 +42,14 @@ class CarDiagnosticsApp extends StatelessWidget {
         ),
 
         // Card widget background color, with a dark shade and subtle neon effect
-        cardColor: Color(0xFF1B1B1F),
+        cardColor: const Color(0xFF1B1B1F),
 
         // Elevated button style settings for consistency across buttons
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFF29B6F6), // Neon blue background for buttons
+            backgroundColor: const Color(0xFF29B6F6), // Neon blue background for buttons
             foregroundColor: Colors.black, // Text color on buttons
-            textStyle: TextStyle(
+            textStyle: const TextStyle(
               fontWeight: FontWeight.bold, // Bold font for button text
               fontSize: 18, // Larger text size for readability
               fontFamily: 'Orbitron', // Consistent font style
@@ -57,13 +57,13 @@ class CarDiagnosticsApp extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10), // Rounded edges for button shape
             ),
-            shadowColor: Color(0xFF29B6F6).withOpacity(0.5), // Neon blue shadow for a glow effect
+            shadowColor: const Color(0xFF29B6F6).withOpacity(0.5), // Neon blue shadow for a glow effect
             elevation: 15, // Elevation to enhance the shadow effect
           ),
         ),
 
         // AppBar (top bar) theme with custom color and text style
-        appBarTheme: AppBarTheme(
+        appBarTheme: const AppBarTheme(
           color: Colors.black, // Dark background matching the app's overall theme
           elevation: 0, // No elevation, making the AppBar flat
           iconTheme: IconThemeData(color: Color(0xFF00FFC6)), // Icon color for AppBar icons (neon green-cyan)
